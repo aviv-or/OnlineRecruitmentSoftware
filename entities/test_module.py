b156015fc43487c9b5493a3d43336f9bcb474995
Shashwat Dixit <shashwatdixit124@gmail.com>
2017-11-22 23:56:47 +0530

    Add remaining time and status related functions to a Test Entity

--- entities/test_module.py
+++ entities/test_module.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from datetime import datetime, date, timedelta
+
 from db.interface.test_module_data import TestModuleData
 from db.problem_set_database import ProblemSetDB
 from db.submission_set_database import SubmissionSetDB
@@ -18,6 +21,82 @@ class TestModule(TestModuleData):
         psetdb = ProblemSetDB.all()
         psets = [doc for doc in psetdb if doc['_id'] in self['problem_sets']]
         return psets
+
+    def status(self):
+        if self.is_completed():
+            return self.Status.COMPLETED
+        elif self.is_live():
+            return self.Status.LIVE
+        else:
+            return self.Status.UPCOMING
+
+    def is_live(self):
+        now = datetime.now()
+
+        if not self.get('schedule'):
+            return False
+
+        test_date = self['schedule']['date']
+        test_time = self['schedule']['time']
+        test_duration = self['schedule']['duration']
+
+        tdatetime = datetime.strptime(test_date+"-"+test_time, "%d-%m-%Y-%H:%M")
+
+        if tdatetime > now:
+            return False
+
+        if tdatetime + timedelta(minutes=int(test_duration)+1) < now:
+            return False
+
+        return True
+
+    def is_completed(self):
+        days, hours, minutes = self.remaining_time(True)
+        if days == 0 and hours == 0 and minutes == 0:
+            return True
+
+        return False
+
+    def pretty_date(self, include_duration=False, year=True):
+        test_date_time = self['schedule']['date']+"-"+self['schedule']['time']
+        test_date_time = datetime.strptime(test_date_time, "%d-%m-%Y-%H:%M")
+
+        if include_duration:
+            test_date_time += timedelta(minutes=int(self['schedule']['duration']))
+
+        if year:
+            return test_date_time.strftime("%d %B %Y")
+        else:
+            return test_date_time.strftime("%d %B")
+
+    def pretty_remaining_time(self):
+        days, hours, minutes = self.remaining_time()
+        result = ""
+        if days != 0:
+            result += str(days) + "d "
+        result += str(hours)+"h "+str(minutes)+"m"
+        return result
+
+    def remaining_time(self, include_duration = False):
+        test_date_time = self['schedule']['date']+"-"+self['schedule']['time']
+        test_date_time = datetime.strptime(test_date_time, "%d-%m-%Y-%H:%M")
+        now = datetime.now()
+
+        if include_duration:
+            end_test_date_time = test_date_time + timedelta(minutes=int(self['schedule']['duration'] ) + 1)
+        else:
+            end_test_date_time = test_date_time
+
+        if now > end_test_date_time:
+            return 0,0,0
+
+        difference = end_test_date_time - now
+
+        days = difference.days
+        hours = difference.seconds//3600 - difference.days*24
+        minutes = difference.seconds//60 - hours*60
+
+        return days, hours, minutes
 
     def submissions(self):
         submissions = self.get('submissions')
@@ -45,3 +124,8 @@ class TestModule(TestModuleData):
                 return False
 
         return True
+
+    class Status(Enum):
+        COMPLETED = "Completed"
+        LIVE = "Live"
+        UPCOMING = "Upcoming"
